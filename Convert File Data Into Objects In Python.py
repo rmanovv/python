2e0eacfd368ec11bc303457a18ba2215c7a0185b
rmanovv <radoslav_m@yahoo.com>
2016-08-23 12:11:02 +0300

    Add files via upload

---
+++ Convert File Data Into Objects In Python.py
@@ -0,0 +1,21 @@
+FILE_PATH = "C:\\Users\Radoslav\\Desktop\\c#\\01.python\\Convert File Data Into Objects In Python\\file.txt"
+FILE_PATH2 = "C:\\Users\Radoslav\\Desktop\\c#\\01.python\\Convert File Data Into Objects In Python\\file2.txt"
+
+text = "This is my string\ndsfd"
+
+with open(FILE_PATH, 'w') as f:
+    f.write(text)
+with open(FILE_PATH, 'r') as file:
+     print(file.read())
+
+
+a = {'Name': 'fds', 'Age': 150}
+
+with open(FILE_PATH2, 'w') as f:
+    f.write(str(a))
+
+with open(FILE_PATH2, 'r') as file:
+    collection = file.readline()
+    print(collection)
+    final = eval(collection)
+    print(type(final))
