7e3dc66f6f4887df26e91dc25a5fc77a66ccb222
Radoslav Manov <radoslav_m@yahoo.com>
2017-05-18 11:45:06 +0300

    Update urls.py

--- django/webapp/music/urls.py
+++ django/webapp/music/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+    # /music/1234...
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
