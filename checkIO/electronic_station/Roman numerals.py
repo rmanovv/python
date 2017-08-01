468b24fa8efc02032243256f83c7510ef3104bbc
Radoslav Manov <radoslav_m@yahoo.com>
2017-08-01 23:26:00 +0300

    Add files via upload

---
+++ checkIO/electronic_station/Roman numerals.py
@@ -0,0 +1,22 @@
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+def checkio(data):
+
+    #replace this for solution
+    roman = ''
+
+    while data > 0:
+        for key, value in num_map:
+            while data >= key:
+                roman += value
+                data -= key
+
+    return roman
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
